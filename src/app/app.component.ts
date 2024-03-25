5a5f395382722bc6aeb67fe86c5be919116ec6d0
Dainovskiene <neringa.dainovskiene@accenture.com>
2024-03-25 18:05:59 +0200

    Implement tarnsitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { state, style, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -12,19 +12,45 @@ import { Component } from '@angular/core';
         transform: 'translateX(0)'
       })),
       state('highlighted', style({
-        backgroundColor: 'red',
+        'background-color': 'palevioletred',
         transform: 'translateX(100px)'
-      }))
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800))
+      // transition('normal <=> highlighted', animate(300))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'pink',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'palevioletred',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(800))
     ])
   ]
 })
 export class AppComponent {
   title = 'animations';
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state =='normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState =='normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken'
   }
 
   onAdd(item) {
